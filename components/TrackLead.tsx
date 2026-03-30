04cbd130f297144b647c57d1c60aed9af592c27f
jyoung905 <jyoung905@gmail.com>
2026-03-30 13:13:34 -0400

    Remove duplicate Google Ads conversion from TrackLead, keep GA4 generate_lead only

--- components/TrackLead.tsx
+++ components/TrackLead.tsx
@@ -13,7 +13,6 @@ export default function TrackLead() {
       event_category: 'intake_form',
       event_label: 'ab_claims_ontario',
     });
-    event('conversion', { send_to: 'AW-18043625605/oKibCNji-ZAcEIXB75tD' });
   }, []);
 
   return null;
