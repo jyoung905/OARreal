371c831a699d80ad76dd01ff1252b74ceb08a581
jyoung905 <jyoung905@gmail.com>
2026-04-02 16:09:20 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/blog', destination: '/blog.html' },
+      { source: '/blog/deadlines', destination: '/blog-deadlines.html' },
+      { source: '/blog/sabs', destination: '/blog-sabs.html' },
+      { source: '/blog/settlement', destination: '/blog-settlement.html' },
+    ];
+  },
+};
 export default nextConfig;
