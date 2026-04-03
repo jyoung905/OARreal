1fd3119112abec24c032c88c55eccfd34d93ea0b
jyoung905 <jyoung905@gmail.com>
2026-04-03 14:56:58 -0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,6 +11,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1.0,
     },
     {
+      url: `${BASE_URL}/income-replacement-denied`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
       url: `${BASE_URL}/contact`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
